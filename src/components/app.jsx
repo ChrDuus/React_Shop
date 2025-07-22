30b8d8c0aabafcf8555f86fc335e6d09c30db7c0
ChrDuus <duus92@web.de>
2025-07-22 15:18:00 +0200

    Cart updates on click on add button

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,21 +5,38 @@ import ShoppingCart from './shopping-card';
 
 
 class App extends Component {
-    state = {  } 
+    state = {
+        items:[]
+      } 
+
+    addItemtoCart = (amount, name, price) =>{
+        let currentItemsInCart = this.state.items;
+        let existingItem = this.state.items.find(item=>item.name === name);
+        if(existingItem){
+            existingItem.amount++;
+        }else{       
+        currentItemsInCart.push({
+            amount,
+            name,
+            price
+        });        
+    }
+    this.setState({items: currentItemsInCart});
+    }
     render() { 
         return (
             <React.Fragment>
                 <NavBar/>
                 <div className="main-container">
                     <div className="product-container">
-                        <Product title="Banana" img="../assets/img/banana.jpg" description="This is a Banana"/>
-                        <Product title= "Cucumber" img="../assets/img/cucumber.jpg" description="This is a Cucumber"/>
-                        <Product title= "Tomatoe" img="../assets/img/tomato.jpg" description="This is a Tomato"/>
-                        <Product title= "Apple" img="../assets/img/apple.jpg" description="This is an Apple"/>
+                        <Product onAdd={() => this.addItemtoCart(1, 'Banana', 1.5)} title="Banana" img="../assets/img/banana.jpg" description="This is a Banana"/>
+                        <Product onAdd={() => this.addItemtoCart(1, 'Cucumber', 0.5)} title= "Cucumber" img="../assets/img/cucumber.jpg" description="This is a Cucumber"/>
+                        <Product onAdd={() => this.addItemtoCart(1, 'Tomatoe', 1.9)} title= "Tomatoe" img="../assets/img/tomato.jpg" description="This is a Tomato"/>
+                        <Product onAdd={() => this.addItemtoCart(1, 'Apple', 2.5)} title= "Apple" img="../assets/img/apple.jpg" description="This is an Apple"/>
 
                     </div>
                     <div className="cart-div">
-                        <ShoppingCart/>
+                        <ShoppingCart items={this.state.items}/>
                     </div>
                 </div>
             </React.Fragment>
--- src/components/shopping-card.jsx
+++ src/components/shopping-card.jsx
@@ -2,35 +2,14 @@ import React, {Component} from 'react';
 
 class ShoppingCart extends Component {
     state = { 
-        items: [
-            {
-                amount:1,
-                name: 'Banana',
-                price: 1.5
-            },
-            {
-                amount:1,
-                name: 'Cucumber',
-                price: 0.5
-            },
-            {
-                amount:1,
-                name: 'Tomatoe',
-                price: 1.9
-            },
-            {
-                amount:1,
-                name: 'Apple',
-                price: 2.5
-            },
-        ]
+       
      } 
 
     render() { 
         return (
             <div className='shopping-cart'>
                 <h2>Cart</h2>
-                {this.state.items.map(item => <div key={item.name}> {item.amount}x {item.name} {item.price}€</div>)}
+                {this.props.items.map(item => <div key={item.name}> {item.amount}x {item.name} {item.price}€</div>)}
             </div>
         );
     }
